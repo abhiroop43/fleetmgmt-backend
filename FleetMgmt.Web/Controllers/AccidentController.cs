9e2f914e5c646874b84504e5dd8103a8b2a69e4c
Abhiroop Santra <abhiroop.santra@gmail.com>
2018-02-26 10:02:37 +0400

    added controller files

---
+++ FleetMgmt.Web/Controllers/AccidentController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FleetMgmt.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Accident")]
+    public class AccidentController : Controller
+    {
+        //// Business Logic: An accident can only happen if the driver is driving the specified vehicle during that time
+    }
+}
---
+++ FleetMgmt.Web/Controllers/TripController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FleetMgmt.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Trip")]
+    public class TripController : Controller
+    {
+        //// Busines Logic: 
+        /// 1. a driver cannot be reserved for multiple trips on the same time period
+        /// 2. a driver cannot drive for more than 10 hrs a day
+        /// 3. a driver must have at least one day off in a week
+    }
+}
