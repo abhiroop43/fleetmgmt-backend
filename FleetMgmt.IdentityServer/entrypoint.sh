4b1b03efd8eb95d5ae226698bb09dd44429a554b
Abhiroop Santra <abhiroop.santra@gmail.com>
2019-07-12 00:24:48 +0400

    fixes

--- FleetMgmt.IdentityServer/entrypoint.sh
+++ FleetMgmt.IdentityServer/entrypoint.sh
@@ -1,12 +1,12 @@
-﻿ #!/bin/bash
+﻿ #!/usr/bin/env bash
 
 set -e
 run_cmd="dotnet FleetMgmt.IdentityServer.dll --server.urls http://*:80"
 
-# until dotnet ef database update --context ConfigurationDbContext; do
-# >&2 echo "SQL Server is starting up"
-# sleep 1
-# done
+until dotnet ef database update --context ConfigurationDbContext; do
+>&2 echo "SQL Server is starting up"
+sleep 1
+done
 
 >&2 echo "SQL Server is up - executing command"
 exec $run_cmd
