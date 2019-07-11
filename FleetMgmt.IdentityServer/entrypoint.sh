337fa5cc5ba59c8bae5d23d0eec79ca2c4285c30
Abhiroop Santra <abhiroop.santra@gmail.com>
2019-07-11 19:07:37 +0400

    added docker support

---
+++ FleetMgmt.IdentityServer/entrypoint.sh
@@ -0,0 +1,12 @@
+﻿ #!/bin/bash
+
+set -e
+run_cmd="dotnet FleetMgmt.IdentityServer.dll --server.urls http://*:80"
+
+# until dotnet ef database update --context ConfigurationDbContext; do
+# >&2 echo "SQL Server is starting up"
+# sleep 1
+# done
+
+>&2 echo "SQL Server is up - executing command"
+exec $run_cmd
